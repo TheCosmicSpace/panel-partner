2de2d054a58860544b49ada21bfdbe2e22102c72
Grant Atayan <atagr888@gmail.com>
2021-01-18 18:08:29 +0300

    create main layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <MainLayoutVue />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MainLayoutVue from './layouts/MainLayout.vue';
+export default defineComponent({
+  components: {
+    MainLayoutVue
+  },
+  name: 'App'
+});
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Uni Neue';
 }
 </style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main-layout">
+    <el-container class="main-layout__container">
+      <el-aside width="320px">Aside</el-aside>
+      <el-container>
+        <el-header>
+          <AppNavMenu />
+        </el-header>
+        <el-main class="main-layout__main">
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view /> -->
+</template>
+
+<script lang="ts">
+import AppNavMenu from '@/components/AppNavMenu.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  components: {
+    AppNavMenu
+  },
+  name: 'MainLayout'
+});
+</script>
+
+<style lang="scss" scoped>
+.el-aside {
+  background-color: #d3dce6;
+}
+.main-layout {
+  height: 100vh;
+  &__container {
+    height: inherit;
+  }
+  &__main {
+    padding: 40px;
+  }
+  .el-header {
+    padding: 0 50px;
+    box-shadow: 0px 0px 31px rgba(0, 0, 0, 0.13);
+  }
+}
+</style>
