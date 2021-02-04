d2352ac4af45fce8047fdbbde0a360d6c581203f
Grant Atayan <atagr888@gmail.com>
2021-02-04 17:10:32 +0300

    add theme color

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 
+import './element-variables.scss';
 import App from './App.vue';
 import router from './router';
 import { store } from './store';
 
-import 'element-plus/lib/theme-chalk/index.css';
 import '@/assets/fonts/stylesheet.css';
 
-createApp(App)
+const app = createApp(App);
+app
   .use(store)
   .use(router)
   .use(ElementPlus)
