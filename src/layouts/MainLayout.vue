919ac1eb855ce6f3424486027a241a0babe966c6
Grant Atayan <atagr888@gmail.com>
2021-02-08 14:07:20 +0300

    add router-view to main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,55 +1,40 @@
 <template>
   <div class="main-layout">
     <el-container class="main-layout__container">
-      <el-aside width="320px">Aside</el-aside>
-      <el-container>
-        <el-header>
-          <AppNavMenu />
-        </el-header>
+      <el-header style="padding: 0">
+        <AppHeader />
+      </el-header>
+
+      <router-view />
+      <!-- <el-container>
+        <OrderList v-if="true" />
         <el-main class="main-layout__main">
-          <router-view></router-view>
+          <OrderPage />
         </el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
+      </el-container> -->
+      <!-- <router-view> -->
     </el-container>
   </div>
-
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view /> -->
 </template>
 
 <script lang="ts">
-import AppNavMenu from '@/components/AppNavMenu.vue';
 import { defineComponent } from 'vue';
+import AppHeader from '@/components/app-header/AppHeader.vue';
+
 export default defineComponent({
   setup() {
     return {};
   },
-  components: {
-    AppNavMenu
-  },
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: { AppHeader }
 });
 </script>
 
 <style lang="scss" scoped>
-.el-aside {
-  background-color: #d3dce6;
-}
 .main-layout {
   height: 100vh;
   &__container {
-    height: inherit;
-  }
-  &__main {
-    padding: 40px;
-  }
-  .el-header {
-    padding: 0 50px;
-    box-shadow: 0px 0px 31px rgba(0, 0, 0, 0.13);
+    height: 100vh;
   }
 }
 </style>
