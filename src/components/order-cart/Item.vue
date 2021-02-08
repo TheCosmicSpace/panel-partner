b1ac124db8d14d6ea4b0960fbfae4bda25e80ce7
Grant Atayan <atagr888@gmail.com>
2021-02-08 14:05:19 +0300

    create order-cart component

---
+++ src/components/order-cart/Item.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="cart-product">
+    <div class="cart-product__info">
+      <div class="cart-product__title">
+        1 × Чизкейк Нью-Йорк
+        <span class="cart-product__weight"> 300 г.</span>
+      </div>
+      <div class="cart-product__price">350 ₽</div>
+    </div>
+    <div class="cart-product-toppings">
+      <div class="cart-product-toppings__title">
+        Халапеньо
+      </div>
+      <div class="cart-product-toppings__price">+15 ₽</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  name: 'OrderCartItem'
+});
+</script>
+
+<style lang="scss" scoped>
+// cart-product
+.cart-product {
+  padding: 15px 0;
+  border-bottom: 2px solid #f1f1f1;
+  &__info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__title {
+    font-weight: normal;
+    font-size: 19px;
+    line-height: 26px;
+  }
+  &__price {
+    font-weight: bold;
+    font-size: 19px;
+    line-height: 26px;
+  }
+  &__weight {
+    color: #9c9c9c;
+  }
+}
+.cart-product-toppings {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  &__title {
+    color: #8e8e8e;
+    font-size: 16px;
+    line-height: 22px;
+  }
+  &__price {
+    font-size: 16px;
+    line-height: 22px;
+  }
+}
+</style>
---
+++ src/components/order-cart/OrderCart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="order-cart">
+    <div class="order-cart-wishes">
+      <div class="order-cart-wishes__person">
+        <i class="el-icon-knife-fork"></i> 4
+      </div>
+      <div class="order-cart-wishes__comment">
+        <i class="el-icon-chat-line-round"></i>
+        Не добавляйте соль, пожалуйста
+      </div>
+    </div>
+    <div class="cart-products order-cart__products">
+      <OrderCartItem />
+      <OrderCartItem />
+      <OrderCartItem />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import OrderCartItem from './Item.vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  name: 'OrderCart',
+  components: { OrderCartItem }
+});
+</script>
+
+<style lang="scss" scoped>
+.order-cart-wishes {
+  margin: 30px 0;
+  display: flex;
+  &__person {
+    font-size: 20px;
+    line-height: 30px;
+    background: #f1f1f1;
+    border-radius: 6px;
+    display: inline-block;
+    padding: 8px 15px;
+    margin-right: 15px;
+  }
+  &__comment {
+    font-size: 20px;
+    line-height: 30px;
+    background: #f1f1f1;
+    border-radius: 6px;
+    padding: 8px 15px;
+    flex: 1;
+  }
+}
+</style>
