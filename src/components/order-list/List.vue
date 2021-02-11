3d9ddfbc89e97a38f7d9767fd7448d00994221ff
Grant Atayan <atagr888@gmail.com>
2021-02-11 19:25:00 +0300

    add skeleton & load order in process

--- src/components/order-list/List.vue
+++ src/components/order-list/List.vue
@@ -1,21 +1,84 @@
 <template>
   <div class="order-list">
-    <OrderGroup group-title="Новые заказы" />
-    <OrderGroup group-title="Готовятся" />
-    <OrderGroup group-title="Переданы курьерам" />
+    <OrderSkeleton :loading="loading" :count="5" animated />
+    <el-empty
+      v-if="isEmptyOrderList && !loading"
+      description="Пока нет заказов"
+    />
+    <template v-else>
+      <template
+        v-for="([title, order], index) in Object.entries(groupsOrder)"
+        :key="index"
+      >
+        <OrderGroup
+          v-if="order.length"
+          :titleGroup="title"
+          :orderGroup="order"
+          group-title="Новые заказы"
+        />
+      </template>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, onMounted, reactive, ref } from 'vue';
+import { useStore } from '@/store';
+import { ActionTypes } from '@/store/order/action-types';
+import { Order } from '@/data/api/model';
+
 import OrderGroup from './Group.vue';
+import OrderSkeleton from './Skeleton.vue';
 
 export default defineComponent({
   setup() {
-    return {};
+    const store = useStore();
+    const groupsOrder = reactive({
+      ready: [] as Order[],
+      delivery: [] as Order[]
+    });
+    const loading = ref(false);
+    const getOrdersInProcess = computed(() => store.getters.getOrdersInProcess);
+    function groupingOrders() {
+      for (const [key, value] of Object.entries(groupsOrder)) {
+        const filtered = getOrdersInProcess.value?.filter(
+          el => el.state === key
+        ) as Order[];
+        console.warn('FILTERED', filtered);
+        console.warn(key, value);
+
+        value.push(...filtered);
+      }
+      console.log(groupsOrder);
+    }
+
+    onMounted(async () => {
+      loading.value = true;
+      await store.dispatch(ActionTypes.FETCH_ORDERS_IN_PROCESS, undefined);
+      loading.value = false;
+      // groupsOrder.ready = getOrdersInProcess.value?.filter(
+      //   el => el.state === 'ready'
+      // ) as Order[];
+      groupingOrders();
+      // groupsOrder.ready = (getOrdersInProcess.value as []) || [];
+      console.warn(groupsOrder);
+    });
+    const isEmptyOrderList = computed(() => {
+      console.warn('===', groupsOrder);
+      return !Object.values(groupsOrder).reduce(
+        (acc, group) => acc + group.length,
+        0
+      );
+    });
+    console.warn(isEmptyOrderList.value, groupsOrder);
+
+    const handleSelectOrder = (order: Order) => {
+      console.log('handleSelectOrder', order);
+    };
+    return { groupsOrder, isEmptyOrderList, handleSelectOrder, loading };
   },
   name: 'OrderList',
-  components: { OrderGroup }
+  components: { OrderGroup, OrderSkeleton }
 });
 </script>
 
