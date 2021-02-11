05bceee7961599bc06f4ba7b9c2738c82eab429b
Grant Atayan <atagr888@gmail.com>
2021-02-11 19:13:59 +0300

    change auth model

--- src/data/api/model/auth.ts
+++ src/data/api/model/auth.ts
@@ -9,14 +9,3 @@ export interface RefreshToken {
   created_at: Date;
   expired: number;
 }
-
-// Converts JSON strings to/from your types
-export class Convert {
-  public static toAuth(json: string): Auth {
-    return JSON.parse(json);
-  }
-
-  public static authToJson(value: Auth): string {
-    return JSON.stringify(value);
-  }
-}
