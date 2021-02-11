7c2e7b3ca0634f588c81c936ad3e475570d23f84
Grant Atayan <atagr888@gmail.com>
2021-02-11 19:30:28 +0300

    create order state machine & load active order

---
+++ src/helpers/order-state-machine.ts
@@ -0,0 +1,22 @@
+export const urlStateMachine = new Map<string, string>([
+  ['created', 'cooking'],
+  ['cooking', 'ready'],
+  ['ready', 'delivery'],
+  ['delivery', 'finish'],
+  ['finish', 'finish'],
+  ['cancel', 'cancel']
+]);
+
+interface TextSM {
+  next: string;
+  cancel: string;
+}
+
+export const textStateMachine = new Map<string, TextSM>([
+  ['created', { next: 'Принять заказ', cancel: 'Отклонить заказ' }],
+  ['cooking', { next: 'Заказ готов', cancel: 'Отменить заказ' }],
+  ['ready', { next: 'Поиск курьера', cancel: 'Отменить заказ' }],
+  ['delivery', { next: 'Заказ передан курьеру', cancel: 'Отменить заказ' }],
+  ['finish', { next: '', cancel: '' }],
+  ['cancel', { next: '', cancel: '' }]
+]);
--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,7 +1,12 @@
 <template>
   <el-container>
     <OrderList />
-    <el-main class="main-layout__main">
+    <el-empty
+      v-if="!activeOrder"
+      style="width: 100%"
+      description="Выберите заказ на панеле слева"
+    ></el-empty>
+    <el-main v-else class="main-layout__main">
       <section class="order-page section">
         <div class="section-tags">
           <el-tag
@@ -12,13 +17,13 @@
           >
             Новый
           </el-tag>
-          <el-tag class="section-tag" type="info" effect="dark">
+          <el-tag class="section-tag" type="info">
             Доставка
           </el-tag>
         </div>
         <hgroup class="order-hgroup">
-          <h1 class="section__title">Выберите заказ на панеле слева</h1>
-          <p class="section__subtitle">Бейрут (ул. Владикавказская, 11)</p>
+          <h1 class="section__title">№{{ activeOrder.id }}</h1>
+          <p class="section__subtitle">{{ activeOrder.store_data.name }}</p>
         </hgroup>
         <div class="order-info">
           <div class="order-info__items">
@@ -39,7 +44,9 @@
             <div class="order-info__item">
               <div class="order-info__item-label">Сумма заказа</div>
               <div class="order-info__item-text">
-                <span class="order-info__sum">715 ₽</span>
+                <span class="order-info__sum">
+                  {{ activeOrder.total_price }} ₽
+                </span>
                 <span>
                   <i class="el-icon-postcard"></i>
                   Оплата ApplePay
@@ -53,24 +60,67 @@
           </div>
         </div>
         <div class="order-controls">
-          <el-button class="order-btn" type="success">Заказ готов</el-button>
-          <el-button class="order-btn" type="info">Отклонить заказ</el-button>
+          <el-button
+            @click="handleNextStepOrder"
+            class="order-btn"
+            type="success"
+          >
+            {{ btnsText.next }}
+          </el-button>
+          <el-button @click="handleCancelOrder" class="order-btn" type="info">
+            {{ btnsText.cancel }}
+          </el-button>
         </div>
         <el-divider></el-divider>
         <!-- OrderCart -->
-        <OrderCart />
+        <OrderCart :cart-items="activeOrder.items" />
       </section>
     </el-main>
   </el-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from '@/store';
+import {
+  urlStateMachine,
+  textStateMachine
+} from '@/helpers/order-state-machine';
+
 import OrderList from '@/components/order-list/List.vue';
 import OrderCart from '@/components/order-cart/OrderCart.vue';
+import { ActionTypes } from '@/store/order/action-types';
 export default defineComponent({
   setup() {
-    return {};
+    const store = useStore();
+    const activeOrder = computed(() => store.getters.getActivedOrder);
+    const btnsText = computed(() => {
+      const state = activeOrder.value?.state;
+      return state ? textStateMachine.get(state) : {};
+    });
+    const handleNextStepOrder = () => {
+      const state = activeOrder.value?.state;
+      if (!state) return;
+      console.log('handleNextStepOrder', urlStateMachine.get(state));
+      store.dispatch(ActionTypes.UPDATE_ORDER_STATE, {
+        uuid: activeOrder.value?.uuid as string,
+        path: urlStateMachine.get(state) as string
+      });
+    };
+    const handleCancelOrder = () => {
+      console.log(
+        'handleCancelOrder',
+        activeOrder.value?.uuid,
+        urlStateMachine.get('cancel')
+      );
+    };
+
+    return {
+      activeOrder,
+      btnsText,
+      handleCancelOrder,
+      handleNextStepOrder
+    };
   },
   name: 'OrderPage',
   components: { OrderList, OrderCart }
@@ -83,7 +133,7 @@ export default defineComponent({
   border-color: #ff3358;
 }
 .section {
-  max-width: 705px;
+  max-width: 1080px;
   &__title {
     font-weight: bold;
     font-size: 29px;
